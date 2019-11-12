23800e9443e636d7b035ff7176204ca023610d3d
ncksllvn <ncksllvn@gmail.com>
2019-11-11 21:25:42 -0500

    Reorganizing

---
+++ src/routes/game.js
@@ -0,0 +1,47 @@
+const graphqlHTTP = require('express-graphql');
+const { buildSchema } = require('graphql');
+
+// Construct a schema, using GraphQL schema language
+const schema = buildSchema(`
+  type RandomDie {
+    numSides: Int!
+    rollOnce: Int!
+    roll(numRolls: Int!): [Int]
+  }
+
+  type Query {
+    getDie(numSides: Int): RandomDie
+  }
+`);
+
+// This class implements the RandomDie GraphQL type
+class RandomDie {
+  constructor(numSides) {
+    this.numSides = numSides;
+  }
+
+  rollOnce() {
+    return 1 + Math.floor(Math.random() * this.numSides);
+  }
+
+  roll({numRolls}) {
+    var output = [];
+    for (var i = 0; i < numRolls; i++) {
+      output.push(this.rollOnce());
+    }
+    return output;
+  }
+}
+
+// The root provides the top-level API endpoints
+const root = {
+  getDie: function ({numSides}) {
+    return new RandomDie(numSides || 6);
+  }
+}
+
+module.exports = graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+});
---
+++ src/routes/index.js
@@ -0,0 +1,5 @@
+function index() {
+  return 200
+}
+
+module.exports = index
