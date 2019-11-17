7ea5e7fb4b697167cb0d0c0a9a5dec04c0cf6adf
ncksllvn <ncksllvn@gmail.com>
2019-11-17 14:26:22 -0500

    Add other game properties

--- src/routes/game/Game.js
+++ src/routes/game/Game.js
@@ -9,24 +9,88 @@ class Game {
     return this.chess.ascii()
   }
 
+  clear() {
+    this.chess.clear()
+  }
+
   fen() {
     return this.chess.fen()
   }
 
-  moves() {
-    return this.chess.moves()
+  gameOver() {
+    return this.chess.game_over()
   }
 
-  inCheck() {
-    return this.chess.in_check()
+  get({ square }) {
+    return this.chess.get(square)
   }
 
   history() {
     return this.chess.history()
   }
 
-  move(move) {
+  inCheck() {
+    return this.chess.in_check()
+  }
+
+  inCheckmate() {
+    return this.chess.in_checkmate()
+  }
+
+  inDraw() {
+    return this.chess.in_draw()
+  }
+
+  inStalemate() {
+    return this.chess.in_stalemate()
+  }
+
+  inThreefoldRepetition() {
+    return this.chess.in_threefold_repetition()
+  }
+
+  header({ headers }) {
+    this.chess.header(headers)
+  }
+
+  insufficientMaterial() {
+    return this.chess.insufficient_material()
+  }
+
+  move({ move }) {
     this.chess.move(move)
+  }
+
+  moves() {
+    return this.chess.moves({ verbose })
+  }
+
+  put({ piece, square }) {
+    return this.chess.put(piece, square)
+  }
+
+  remove({ square }) {
+    return this.chess.remove(square)
+  }
+
+  reset() {
+    this.chess.reset()
+  }
+
+  squareColor({ square }) {
+    return this.chess.square_color(square)
+  }
+
+  turn() {
+    return this.chess.turn()
+  }
+
+  undo() {
+    return this.chess.undo()
+  }
+
+  validateFen({ fen }) {
+    return this.chess.validate_fen(fen)
   }
 }
 
