4a98c912428d56b1f3fa791242e1ff83061ff0ea
ncksllvn <ncksllvn@gmail.com>
2019-11-18 21:52:46 -0500

    Move engine to root

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 const config = {
-  ENGINE: path.join(__dirname, '../engines', process.env.ENGINE)
+  ENGINE: path.join(__dirname, '../../engines', process.env.ENGINE)
 }
 
 module.exports = config
