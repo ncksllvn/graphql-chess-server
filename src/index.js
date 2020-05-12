517cb2145f87bc45dc1769cd233c6de5fd50b535
ncksllvn <ncksllvn@gmail.com>
2020-05-12 18:19:06 -0400

    Simplify workflow by removing multiple routes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
 require('dotenv').config()
 
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const { buildSchema } = require('graphql');
+
+const engine = require('./utilities/engine')
+const Game = require('./models/Game')
+
+const schema = buildSchema(
+  fs.readFileSync(
+    path.join(__dirname, 'schema.graphql')
+  ).toString()
+)
 
 async function main() {
-  const app = express();
+  const app = express()
 
-  app.use('/game', await require('./routes/game')())
-  app.use('/', require('./routes/index'))
+  await engine.initialize()
+
+  app.use('/',
+    graphqlHTTP({
+      graphiql: true,
+      schema,
+      rootValue: {
+        game({ fen }) {
+          return new Game(fen, engine)
+        },
+        makeMove({ input: { fen, move } }) {
+          const game = new Game(fen, engine)
+          game.move(move)
+          return game
+        }
+      },
+    })
+  )
+
   app.listen(4000);
 }
 
--- src/routes/index.js
+++
@@ -1,5 +0,0 @@
-function index(req, res) {
-  res.send(200)
-}
-
-module.exports = index
