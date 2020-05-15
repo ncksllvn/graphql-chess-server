a584159a12c84deee8ac3a70d5af200c37af001b
ncksllvn <ncksllvn@gmail.com>
2020-05-15 11:05:41 -0400

    WIP - use a proxy to access chessjs methods

--- src/models/Game.js
+++ src/models/Game.js
@@ -1,18 +1,51 @@
 const { Chess } = require('chess.js')
 const log = require('../utilities/log')('model/Game')
 
+const handler = {
+  get(target, prop) {
+    const map = {
+      'ascii': () => target.chess.ascii(),
+      'fen': () => target.chess.fen(),
+      'gameOver': () => target.chess.game_over(),
+      'inCheck': () => target.chess.in_check(),
+      'inCheckmate': () => target.chess.in_checkmate(),
+      'inDraw': () => target.chess.in_draw(),
+      'inStalemate': () => target.chess.in_stalemate(),
+      'inThreefoldRepetition': () => target.chess.in_threefold_repetition(),
+      'moves': () => target.chess.moves({ verbose: true }),
+      'move': (move) => target.chess.move(move),
+      'turn': () => target.chess.turn(),
+      'constants': () => {
+        return {
+          BISHOP: target.chess.BISHOP,
+          BLACK: target.chess.BLACK,
+          FLAGS: target.chess.FLAGS,
+          KING: target.chess.KING,
+          KNIGHT: target.chess.KNIGHT,
+          PAWN: target.chess.PAWN,
+          QUEEN: target.chess.QUEEN,
+          ROOK: target.chess.ROOK,
+          SQUARES: target.chess.SQUARES,
+          WHITE: target.chess.WHITE
+        }
+      }
+    }
+
+    if (prop in map) return map[prop]()
+
+    return target[prop]
+  }
+}
+
 class Game {
   constructor(fen, engine) {
-    this.engine = engine
     this.chess = new Chess(fen)
-  }
-
-  ascii() {
-    return this.chess.ascii()
+    this.engine = engine
+    return new Proxy(this, handler)
   }
 
   async bestMove() {
-    const fen = this.fen()
+    const fen = this.fen
     const depth = 1
 
     log(`calculating best move for FEN ${fen} at depth ${depth}`)
@@ -40,65 +73,6 @@ class Game {
       to: `${toColumn}${toRow}`,
       flags: flags.join('')
     }
-  }
-
-  constants() {
-    return {
-      BISHOP: this.chess.BISHOP,
-      BLACK: this.chess.BLACK,
-      FLAGS: this.chess.FLAGS,
-      KING: this.chess.KING,
-      KNIGHT: this.chess.KNIGHT,
-      PAWN: this.chess.PAWN,
-      QUEEN: this.chess.QUEEN,
-      ROOK: this.chess.ROOK,
-      SQUARES: this.chess.SQUARES,
-      WHITE: this.chess.WHITE
-    }
-  }
-
-  fen() {
-    return this.chess.fen()
-  }
-
-  gameOver() {
-    return this.chess.game_over()
-  }
-
-  inCheck() {
-    return this.chess.in_check()
-  }
-
-  inCheckmate() {
-    return this.chess.in_checkmate()
-  }
-
-  inDraw() {
-    return this.chess.in_draw()
-  }
-
-  inStalemate() {
-    return this.chess.in_stalemate()
-  }
-
-  inThreefoldRepetition() {
-    return this.chess.in_threefold_repetition()
-  }
-
-  insufficientMaterial() {
-    return this.chess.insufficient_material()
-  }
-
-  move(move) {
-    this.chess.move(move)
-  }
-
-  moves() {
-    return this.chess.moves({ verbose: true })
-  }
-
-  turn() {
-    return this.chess.turn()
   }
 }
 
