5b2210a0a3b2f34cc881fbab0ce6daa329e31d19
ncksllvn <ncksllvn@gmail.com>
2020-05-15 14:38:52 -0400

    Add log statements

--- src/root.js
+++ src/root.js
@@ -4,16 +4,17 @@ const log = require('./utilities/log')('root')
 function getRoot(engine) {
   return {
     constants() {
+      log('constants')
       return new Chess().constants
     },
 
     chess({ fen }) {
-      log(`executing game with fen "${fen}"`)
+      log(`chess - fen "${fen}"`)
       return new Chess(fen, engine)
     },
 
     makeMove({ input: { fen, move } }) {
-      log(`executing makeMove with fen "${fen}", move "${move.from}${move.to}"`)
+      log(`makeMove - fen "${fen}", move "${move.from}${move.to}"`)
       const chess = new Chess(fen, engine)
       chess.move(move)
       return chess
