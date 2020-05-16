bc774f6a6e718343a2f46d8af72277753eebec1f
ncksllvn <ncksllvn@gmail.com>
2020-05-16 18:13:05 -0400

    Normalize instead of proxy

--- src/models/Chess.js
+++ src/models/Chess.js
@@ -2,37 +2,32 @@ const { Chess: ChessJS } = require('chess.js')
 const Analysis = require('./Analysis')
 const log = require('../utilities/log')('model/Chess')
 
-const aliases = new Map([
-  ['gameOver', 'game_over'],
-  ['inCheck', 'in_check'],
-  ['inCheckmate', 'in_checkmate'],
-  ['inDraw', 'in_draw'],
-  ['inStalemate', 'in_stalemate'],
-  ['insufficientMaterial', 'insufficient_material'],
-  ['inThreefoldRepetition', 'in_threefold_repetition'],
-])
-
-function get(engine, target, key) {
-  if (key == 'moves') {
-    return target.moves({ verbose: true })
-  }
-
-  if (key == 'analysis') {
-    return new Analysis(engine, target.fen()).results()
-  }
-
-  const translatedKey = aliases.has(key) ? aliases.get(key) : key
-  const reflected = Reflect.get(target, translatedKey)
-
-  log(`${key} -> target.${translatedKey}`)
-
-  return reflected
+const rewrite = {
+  gameOver: 'game_over',
+  inCheck: 'in_check',
+  inCheckmate: 'in_checkmate',
+  inDraw: 'in_draw',
+  inStalemate: 'in_stalemate',
+  insufficientMaterial: 'insufficient_material',
+  inThreefoldRepetition: 'in_threefold_repetition',
 }
 
 function Chess(fen, engine) {
   const chess = new ChessJS(fen)
-  const handler = { get: get.bind(null, engine) }
-  return new Proxy(chess, handler)
+  const keys = Object.keys(chess)
+
+  const normalized = keys.reduce((result, key) => {
+    return {
+      [key in rewrite ? rewrite[key] : key]: chess[key],
+      ...result
+    }
+  }, {})
+
+  return {
+    ...normalized,
+    analysis: () => new Analysis(engine, chess.fen()).results(),
+    moves: () => chess.moves({ verbose: true })
+  }
 }
 
 module.exports = Chess
