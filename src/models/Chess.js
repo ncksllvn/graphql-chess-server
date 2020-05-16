2bf02fd2491c0454c2f7d2f46f41468c40e07c1a
ncksllvn <ncksllvn@gmail.com>
2020-05-16 18:23:43 -0400

    normalize

--- src/models/Chess.js
+++ src/models/Chess.js
@@ -2,31 +2,36 @@ const { Chess: ChessJS } = require('chess.js')
 const Analysis = require('./Analysis')
 const log = require('../utilities/log')('model/Chess')
 
-const rewrite = {
-  gameOver: 'game_over',
-  inCheck: 'in_check',
-  inCheckmate: 'in_checkmate',
-  inDraw: 'in_draw',
-  inStalemate: 'in_stalemate',
-  insufficientMaterial: 'insufficient_material',
-  inThreefoldRepetition: 'in_threefold_repetition',
+const normalization = new Map([
+  ['game_over', 'gameOver'],
+  ['in_check', 'inCheck'],
+  ['in_checkmate', 'inCheckmate'],
+  ['in_draw', 'inDraw'],
+  ['in_stalemate', 'inStalemate'],
+  ['insufficient_material', 'insufficientMaterial'],
+  ['in_threefold_repetition', 'inThreefoldRepetition']
+])
+
+function normalizeKeys(chess) {
+  const keys = Object.keys(chess)
+
+  return keys.reduce((result, key) => {
+    const translated = normalization.has(key) ? normalization.get(key) : key
+    return {
+      [translated]: chess[key],
+      ...result
+    }
+  }, {})
 }
 
 function Chess(fen, engine) {
   const chess = new ChessJS(fen)
-  const keys = Object.keys(chess)
-
-  const normalized = keys.reduce((result, key) => {
-    return {
-      [key in rewrite ? rewrite[key] : key]: chess[key],
-      ...result
-    }
-  }, {})
+  const normalized = normalizeKeys(chess)
 
   return {
     ...normalized,
-    analysis: () => new Analysis(engine, chess.fen()).results(),
-    moves: () => chess.moves({ verbose: true })
+    analysis: () => new Analysis(engine, normalized.fen()).results(),
+    moves: () => normalized.moves({ verbose: true })
   }
 }
 
