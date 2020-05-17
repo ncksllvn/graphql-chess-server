34eb0665befd8e2680ca3365607b08dfc18ce1bf
ncksllvn <ncksllvn@gmail.com>
2020-05-16 20:10:50 -0400

    Alias but don't rewrite methods

--- src/models/Chess.js
+++ src/models/Chess.js
@@ -2,31 +2,30 @@ const { Chess: ChessJS } = require('chess.js')
 const Analysis = require('./Analysis')
 const log = require('../utilities/log')('model/Chess')
 
-const normalization = new Map([
-  ['game_over', 'gameOver'],
-  ['in_check', 'inCheck'],
-  ['in_checkmate', 'inCheckmate'],
-  ['in_draw', 'inDraw'],
-  ['in_stalemate', 'inStalemate'],
-  ['insufficient_material', 'insufficientMaterial'],
-  ['in_threefold_repetition', 'inThreefoldRepetition']
-])
+const aliases = {
+  gameOver: 'game_over',
+  inCheck: 'in_check',
+  inCheckmate: 'in_checkmate',
+  inDraw: 'in_draw',
+  inStalemate: 'in_stalemate',
+  insufficientMaterial: 'insufficient_material',
+  inThreefoldRepetition: 'in_threefold_repetition'
+}
 
-function normalizeKeys(chess) {
-  const keys = Object.keys(chess)
-
-  return keys.reduce((result, key) => {
-    const normalized = normalization.has(key) ? normalization.get(key) : key
-    return {
-      [normalized]: chess[key],
-      ...result
-    }
-  }, {})
+function aliasMethods(chess) {
+  return Object
+    .entries(aliases)
+    .reduce((result, [alias, key]) => {
+      return {
+        [alias]: chess[key],
+        ...result
+      }
+    }, chess)
 }
 
 function Chess(fen, engine) {
   const chess = new ChessJS(fen)
-  const normalized = normalizeKeys(chess)
+  const normalized = aliasMethods(chess)
 
   return {
     ...normalized,
