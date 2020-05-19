168e8c079d13f4822a03d5e924633185bf1b63cb
ncksllvn <ncksllvn@gmail.com>
2020-05-19 18:04:06 -0400

    Super rough comand queue

--- src/engine.js
+++ src/engine.js
@@ -1,18 +1,8 @@
 const log = require('./utilities/log')
 const Stockfish = require('stockfish')
 
-let stockfish
-
+// UCI protocol -
 // http://wbec-ridderkerk.nl/html/UCIProtocol.html
-const COMMANDS = {
-  NEW_GAME: 'ucinewgame',
-  SWITCH_TO_UCI_MODE: 'uci',
-  POSITION: 'position'
-}
-
-const RESPONSES = {
-  READY: 'uciok'
-}
 
 const logger = {
   log: log('engine:'),
@@ -20,28 +10,70 @@ const logger = {
   sent: log('engine:sent')
 }
 
-function postMessage(message){
-  logger.sent(message)
-  stockfish.postMessage(message)
-}
+class Engine {
+  constructor() {
+    this.commandQueue = []
+    this.stockfish = new Stockfish()
+    this.stockfish.onmessage = this.onMessage
+  }
 
-function initialize() {
-  stockfish = new Stockfish()
-
-  return new Promise((resolve) => {
-    stockfish.onmessage = (message) => {
-      logger.received(message)
-
-      if (message === RESPONSES.READY) {
-        logger.log('engine is initialized')
-        resolve()
-      }
+  onMessage = (message) => {
+    logger.received(message)
+    if (this.commandQueue.length) {
+      const [{ onMessage }] = this.commandQueue
+      onMessage(message)
     }
+  }
 
-    postMessage(COMMANDS.SWITCH_TO_UCI_MODE)
+  postMessage(message) {
+    logger.sent(message)
+    this.stockfish.postMessage(message)
+  }
+
+  issueCommand({ command, listener }) {
+    return new Promise(resolve => {
+      const onMessage = message => {
+        if (listener(message)) {
+          this.commandQueue.pop()
+          if (this.commandQueue.length) {
+            const [{ command }] = this.commandQueue
+            this.postMessage(command)
+          }
+          resolve()
+        }
+      }
+
+      this.commandQueue.push({
+        command,
+        onMessage
+      })
+
+      if (this.commandQueue.length === 1) {
+        this.postMessage(command)
+      }
+    })
+  }
+
+  findBestMove(fen) {
+
+  }
+}
+
+(async () => {
+  const engine = new Engine
+
+  await engine.issueCommand({
+    command: 'uci',
+    listener: message => message == 'uciok'
   })
-}
 
-module.exports = {
-  initialize
-}
+  engine.postMessage('ucinewgame')
+  engine.postMessage('position "rnbqkbnr/pppppppp/8/8/8/P7/1PPPPPPP/RNBQKBNR b KQkq - 0 1"')
+
+  await engine.issueCommand({
+    command: 'go depth 1',
+    listener: () => true
+  })
+
+
+})()
